3fad59249104a431e46ec22653887030526db4ba
nicolesano <nicole.honyaku@gmail.com>
2019-12-27 18:46:26 +0900

    Basic journal controller functions

---
+++ app/controllers/journals_controller.rb
@@ -0,0 +1,26 @@
+class JournalsController < ApplicationController
+    def index
+        @journals = Journal.all
+    end
+
+    def new
+        @journal = Journal.new
+    end
+
+    def show
+        @onsen = Journal.find(params[:id])
+    end
+
+    def create
+        @journal = Journal.new(journal_params)
+        if @journal.save
+        redirect_to journal_path(@journal)
+        else
+        render :new
+        end
+    end
+
+    def journal_params
+        params.require(:journal).permit(:name)
+    end    
+end
