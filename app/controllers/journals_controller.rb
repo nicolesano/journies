dd7c661798a87bf7f7f832abcd25dda7eb17c80b
nicolesano <nicole.honyaku@gmail.com>
2019-12-29 23:51:35 +0900

    Quotes section

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,4 +1,5 @@
 class JournalsController < ApplicationController
+
     before_action :authenticate_user!, except: [:index, :show]
 
     def index
@@ -9,7 +10,8 @@ class JournalsController < ApplicationController
         @journal = current_user.journals.build
     end
 
-    def show
+    def show    
+        @default_quotes = ["\"Those who stay will be champions.\" - Bo Schembechler", "\"I haven’t failed. I’ve just found 10,000 ways that won’t work.\” - Thomas Edison"]
         @journal = Journal.find(params[:id])
     end
 
